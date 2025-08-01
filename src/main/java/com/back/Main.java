8366c9bb4f53538b598d875be80b2809557b68ea
chacha86 <taejincha0619@gmail.com>
2025-08-01 15:34:24 +0900

    work

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -8,7 +8,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
-        sc.nextLine(); // 입력값 받기
+
+        while(true) {
+            System.out.print("명령) ");
+            String command = sc.nextLine();
+
+            if(command.equals("등록")) {
+                System.out.print("명언 : ");
+                sc.nextLine();
+                System.out.print("작가 : ");
+                sc.nextLine();
+            } else if(command.equals("종료")) {
+                break;
+            }
+        }
     }
 }

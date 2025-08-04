332cd85683a89a5581e9adacea7da2938963524c
chacha86 <taejincha0619@gmail.com>
2025-08-04 10:17:05 +0900

    work

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -6,11 +6,10 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        int lastNo = 0;
+        WiseSaying wiseSaying = null; // 비어 있는 WiseSaying 변수 선언
 
         System.out.println("== 명언 앱 ==");
-        int lastNo = 0;
-        String wiseSaying = "";
-        String author = "";
 
         while(true) {
             System.out.print("명령) ");
@@ -18,17 +17,23 @@ public class Main {
 
             if(command.equals("등록")) {
                 System.out.print("명언 : ");
-                wiseSaying = sc.nextLine();
+                String saying = sc.nextLine();
                 System.out.print("작가 : ");
-                author = sc.nextLine();
+                String author = sc.nextLine();
 
                 lastNo++;
+
+                wiseSaying = new WiseSaying();
+                wiseSaying.id = lastNo;
+                wiseSaying.saying = saying;
+                wiseSaying.author = author;
+
                 System.out.println("%d번 명언이 등록되었습니다.".formatted(lastNo));
             } else if(command.equals("목록")) {
 
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                System.out.println("%d / %s / %s".formatted(lastNo, wiseSaying, author));
+                System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.saying, wiseSaying.author));
 
 
             } else if(command.equals("종료")) {
@@ -37,3 +42,4 @@ public class Main {
         }
     }
 }
+

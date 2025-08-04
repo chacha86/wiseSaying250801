701915f1aa27e0feab1e0171f4f07eaa42a6619b
chacha86 <taejincha0619@gmail.com>
2025-08-04 10:47:37 +0900

    work

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -37,7 +37,7 @@ public class Main {
 
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                for(int i = 0; i < wiseSayings.length; i++) {
+                for(int i = lastIndex - 1; i >= 0; i--) {
                     WiseSaying target = wiseSayings[i];
 
                     if(target == null) {

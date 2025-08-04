020e738336e32203d2b2221afb45587d733de3fc
chacha86 <taejincha0619@gmail.com>
2025-08-04 09:47:13 +0900

    work

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -8,7 +8,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("== 명언 앱 ==");
-        int no = 1;
+        int lastNo = 0;
+        String wiseSaying = "";
+        String author = "";
 
         while(true) {
             System.out.print("명령) ");
@@ -16,12 +18,19 @@ public class Main {
 
             if(command.equals("등록")) {
                 System.out.print("명언 : ");
-                sc.nextLine();
+                wiseSaying = sc.nextLine();
                 System.out.print("작가 : ");
-                sc.nextLine();
+                author = sc.nextLine();
 
-                System.out.println("%d번 명언이 등록되었습니다.".formatted(no));
-                no++;
+                lastNo++;
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(lastNo));
+            } else if(command.equals("목록")) {
+
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                System.out.println("%d / %s / %s".formatted(lastNo, wiseSaying, author));
+
+
             } else if(command.equals("종료")) {
                 break;
             }

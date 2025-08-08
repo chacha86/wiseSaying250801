6dffc83bfc1f0f3fece4ded089616888b64b1618
chacha86 <taejincha0619@gmail.com>
2025-08-08 11:49:32 +0900

    work

--- src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
@@ -1,9 +1,10 @@
-package com.back.domain.wiseSaying;
+package com.back.domain.wiseSaying.entity;
 
 import java.time.LocalDateTime;
 
 // 첫 글자 대문자. - 파스칼 표기법
 // 의미가 달라지는 부분 시작 글자 대문자. - 카멜 표기법
+// 엔터티
 public class WiseSaying {
 
     private int id;
